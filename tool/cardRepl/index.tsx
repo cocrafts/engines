66221f1b7db1264e27947a6aab35a6a6f011c64e
kayaba <kayaba.fluctlight@gmail.com>
2022-06-17 06:08:35 +0700

    hot render Game board once data change

--- tool/cardRepl/index.tsx
+++ tool/cardRepl/index.tsx
@@ -1,5 +1,11 @@
 import { render } from 'ink';
+import { subscribe } from 'valtio';
 
 import Duel from './Duel';
+import { game } from './state';
 
-render(<Duel />);
+render(<Duel game={game} />);
+
+subscribe(game, () => {
+	render(<Duel game={game} />);
+});

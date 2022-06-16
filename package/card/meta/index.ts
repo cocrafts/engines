17df8bdd22fcfbfe77aa1677b5027292020f885a
kayaba <kayaba.fluctlight@gmail.com>
2022-06-17 02:43:47 +0700

    port meta helper from legacy repo

---
+++ package/card/meta/index.ts
@@ -0,0 +1,8 @@
+import { GameMeta } from '../types';
+
+import { metaFromConfig } from './helper';
+import { initialCardConfigs } from './initial';
+
+export const fetchGameMeta = (version: string): GameMeta => {
+	return metaFromConfig(initialCardConfigs, version);
+};

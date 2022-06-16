14c098f749083f955bf080d720889171501804f4
kayaba <kayaba.fluctlight@gmail.com>
2022-06-17 06:08:12 +0700

    bind official data shape to Duel (Game) component

--- tool/cardRepl/Duel.tsx
+++ tool/cardRepl/Duel.tsx
@@ -1,41 +1,57 @@
 import { FC } from 'react';
 import { CardState, DuelState } from '@cocrafts/card';
 import { Box } from 'ink';
-import { useSnapshot } from 'valtio';
 
 import Card from './components/Card';
 import Deck from './components/Deck';
 import Player from './components/Player';
-import { game } from './state';
+import { initialState } from './state';
 
-export const Duel: FC = () => {
-	const { player, deck } = useSnapshot<DuelState>(game);
+const cardHeight = 13;
+
+interface Props {
+	game: DuelState;
+}
+
+export const Duel: FC<Props> = ({ game }) => {
+	const { player, deck, hand, ground } = game;
 	const [firstColor, secondColor] = ['blue', 'red'];
 	const [firstPlayer, secondPlayer] = player;
 	const [firstDeck, secondDeck] = deck;
+	const [startedFirstDeck, startedSecondDeck] = initialState.deck;
+	const [firstHand, secondHand] = hand;
+	const [firstGround, secondGround] = ground;
 
 	return (
 		<Box flexDirection="column">
 			<Player state={secondPlayer} />
-			<Box justifyContent="center">
-				{secondDeck.slice(11, 15).map((card, i) => (
+			<Box justifyContent="center" height={cardHeight}>
+				{secondHand.map((card, i) => (
 					<Card color={secondColor} item={card} key={i} />
 				))}
 			</Box>
-			<Deck color={secondColor} deck={secondDeck as CardState[]} />
-			<Box justifyContent="center">
-				{firstDeck.slice(0, 11).map((card, i) => (
+			<Deck
+				color={secondColor}
+				deck={secondDeck as CardState[]}
+				startedDeck={startedSecondDeck}
+			/>
+			<Box justifyContent="center" height={cardHeight}>
+				{secondGround.map((card, i) => (
 					<Card color={secondColor} item={card} key={i} />
 				))}
 			</Box>
-			<Box justifyContent="center">
-				{secondDeck.slice(0, 11).map((card, i) => (
+			<Box justifyContent="center" height={cardHeight}>
+				{firstGround.map((card, i) => (
 					<Card color={firstColor} item={card} key={i} />
 				))}
 			</Box>
-			<Deck color={firstColor} deck={firstDeck as CardState[]} />
-			<Box justifyContent="center">
-				{firstDeck.slice(11, 15).map((card, i) => (
+			<Deck
+				color={firstColor}
+				deck={firstDeck as CardState[]}
+				startedDeck={startedFirstDeck}
+			/>
+			<Box justifyContent="center" height={cardHeight}>
+				{firstHand.map((card, i) => (
 					<Card color={firstColor} item={card} key={i} />
 				))}
 			</Box>

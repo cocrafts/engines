f6d21155210562fe052f4018ab186375220453bd
kayaba <fluctlight-kayaba@gmail.com>
2022-12-29 09:38:27 +0700

    keep grave card in sync

--- tool/cardRepl/components/GraveYard/GraveCard.tsx
+++ tool/cardRepl/components/GraveYard/GraveCard.tsx
@@ -1,24 +1,24 @@
 import { FC } from 'react';
-import { CardType, string } from '@metacraft/murg-engine';
+import { CardType } from '@metacraft/murg-engine';
 import { Box, Text } from 'ink';
 import { useSnapshot } from 'valtio';
 
 import { duel } from '../../util';
 
 interface Props {
-	item: string;
+	id: string;
 }
 
-export const GraveCard: FC<Props> = ({ item }) => {
+export const GraveCard: FC<Props> = ({ id }) => {
 	const { cardMap } = useSnapshot(duel);
-	const card = cardMap[item.id.substring(0, 9)];
+	const card = cardMap[id.substring(0, 9)];
 	const isSpell = card.kind === CardType.Spell;
 	const dotColor = isSpell ? 'green' : '#282828';
 
 	return (
 		<Box>
 			<Text color={dotColor}> • </Text>
-			<Text color="#323232">{item.id.substring(2, 4)}</Text>
+			<Text color="#323232">{id.substring(2, 4)}</Text>
 		</Box>
 	);
 };

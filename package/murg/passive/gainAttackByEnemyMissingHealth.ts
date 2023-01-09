8ba8d07184537d55c2d0b063d1e053491241156e
FME849 <1414439@gmail.com>
2023-01-09 14:30:59 +0700

    Refactor passive skills

--- package/murg/passive/gainAttackByEnemyMissingHealth.ts
+++ package/murg/passive/gainAttackByEnemyMissingHealth.ts
@@ -11,12 +11,13 @@ export const gainAttackByEnemyMissingHealth: PassiveRunner = ({
 	const facingIdentifier = getFacingIdentifier(duel, state.owner, cardId);
 	const facingCard = getCard(duel.cardMap, facingIdentifier.id);
 	const facingState = getCardState(duel.stateMap, facingIdentifier.id);
+	const missingHealth = !facingState?.id
+		? 0
+		: facingCard.attribute.health - facingState.health;
 
 	return [
 		{
-			attack: facingIdentifier?.id
-				? facingCard.attribute.health - (facingState?.health || 0)
-				: 0,
+			attack: Math.max(0, missingHealth),
 			health: 0,
 			defense: 0,
 		},

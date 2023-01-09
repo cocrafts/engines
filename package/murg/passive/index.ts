ee0b8d36c955601cb46bb533ddbed93870ecb2b7
FME849 <1414439@gmail.com>
2023-01-09 16:39:49 +0700

    Add new GainAttackByRemainingHealth passive skill

--- package/murg/passive/index.ts
+++ package/murg/passive/index.ts
@@ -2,10 +2,12 @@ import { PassiveIds, PassiveRunner } from '../utils/type';
 
 import { gainAttackByEnemyDefense } from './gainAttackByEnemyDefense';
 import { gainAttackByEnemyMissingHealth } from './gainAttackByEnemyMissingHealth';
+import { gainAttackByRemainingHealth } from './gainAttackByRemainingHealth';
 import { ignoreEnemyDefense } from './ignoreEnemyDefense';
 
 export const passiveMap: Record<PassiveIds, PassiveRunner> = {
 	GainAttackByEnemyDefense: gainAttackByEnemyDefense,
 	IgnoreEnemyDefense: ignoreEnemyDefense,
 	GainAttackByEnemyMissingHealth: gainAttackByEnemyMissingHealth,
+	GainAttackByRemainingHealth: gainAttackByRemainingHealth,
 };

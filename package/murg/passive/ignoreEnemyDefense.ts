f90fa074a0b51311887fe27310052e75ee38abdd
FME849 <1414439@gmail.com>
2023-01-11 17:21:14 +0700

    Refactor ignoreEnemyDefense passive skill to handle specific case

--- package/murg/passive/ignoreEnemyDefense.ts
+++ package/murg/passive/ignoreEnemyDefense.ts
@@ -1,20 +1,33 @@
-import { getCardState } from '../utils/card';
+import { getCard, getCardState } from '../utils/card';
 import { getFacingIdentifier } from '../utils/ground';
 import { emptyPassive } from '../utils/helper';
 import { PassiveRunner } from '../utils/type';
 
+interface BasicAttributes {
+	defense: number;
+}
+
 export const ignoreEnemyDefense: PassiveRunner = ({ duel, cardId }) => {
+	const card = getCard(duel.cardMap, cardId);
+	const { defense }: BasicAttributes = card.skill.passiveAttribute as never;
 	const state = getCardState(duel.stateMap, cardId);
 	const facingIdentifier = getFacingIdentifier(duel, state.owner, cardId);
 	const facingState = getCardState(duel.stateMap, facingIdentifier.id);
 	const facingDefense = facingState?.defense || 0;
+	let finalDefense = 0;
+
+	if (defense) {
+		finalDefense = Math.max(0, defense);
+	} else {
+		finalDefense = Math.max(0, facingDefense);
+	}
 
 	return [
 		emptyPassive,
 		{
 			attack: 0,
 			health: 0,
-			defense: -Math.max(0, facingDefense),
+			defense: -finalDefense,
 		},
 	];
 };

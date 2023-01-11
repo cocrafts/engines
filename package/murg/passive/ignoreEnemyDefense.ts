a0c7f4df0af5f347570b9f8c8f0f9920e64f7ce2
kayaba <fluctlight-kayaba@gmail.com>
2023-01-11 17:48:45 +0700

    refactor ignoreEnemyDefense passive

--- package/murg/passive/ignoreEnemyDefense.ts
+++ package/murg/passive/ignoreEnemyDefense.ts
@@ -1,7 +1,7 @@
 import { getCard, getCardState } from '../utils/card';
 import { getFacingIdentifier } from '../utils/ground';
 import { emptyPassive } from '../utils/helper';
-import { PassiveRunner } from '../utils/type';
+import { Attribute, PassiveRunner } from '../utils/type';
 
 interface BasicAttributes {
 	defense: number;
@@ -10,24 +10,22 @@ interface BasicAttributes {
 export const ignoreEnemyDefense: PassiveRunner = ({ duel, cardId }) => {
 	const card = getCard(duel.cardMap, cardId);
 	const { defense }: BasicAttributes = card.skill.passiveAttribute as never;
-	const state = getCardState(duel.stateMap, cardId);
-	const facingIdentifier = getFacingIdentifier(duel, state.owner, cardId);
-	const facingState = getCardState(duel.stateMap, facingIdentifier.id);
-	const facingDefense = facingState?.defense || 0;
-	let finalDefense = 0;
+	const enemyPassive: Attribute = {
+		attack: 0,
+		health: 0,
+		defense: 0,
+	};
 
 	if (defense) {
-		finalDefense = Math.max(0, defense);
+		enemyPassive.defense = Math.max(0, defense);
 	} else {
-		finalDefense = Math.max(0, facingDefense);
+		const state = getCardState(duel.stateMap, cardId);
+		const facingIdentifier = getFacingIdentifier(duel, state.owner, cardId);
+		const facingState = getCardState(duel.stateMap, facingIdentifier.id);
+		const facingDefense = facingState?.defense || 0;
+
+		enemyPassive.defense = Math.max(0, facingDefense);
 	}
 
-	return [
-		emptyPassive,
-		{
-			attack: 0,
-			health: 0,
-			defense: -finalDefense,
-		},
-	];
+	return [emptyPassive, enemyPassive];
 };

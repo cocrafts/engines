96a847343078b17a9019806289dff500c4d7b1c8
FME849 <1414439@gmail.com>
2023-01-12 14:10:39 +0700

    Add mutateByClass passive skill

---
+++ package/murg/passive/mutateByClass.ts
@@ -0,0 +1,28 @@
+import { getCard, getCardState } from '../utils/card';
+import { getFacingIdentifier } from '../utils/ground';
+import { emptyPassive } from '../utils/helper';
+import { Attribute, ClassType, PassiveRunner } from '../utils/type';
+
+type SkillOptions = Omit<Attribute, 'charge'> & {
+	isTargetEnemy: boolean;
+	classTypes: ClassType[];
+};
+
+export const mutateByClass: PassiveRunner = ({ duel, cardId }) => {
+	const card = getCard(duel.cardMap, cardId);
+	const attr: SkillOptions = card.skill.passiveAttribute as never;
+	const state = getCardState(duel.stateMap, cardId);
+	const facingIdentifier = getFacingIdentifier(duel, state.owner, cardId);
+	const facingCard = getCard(duel.cardMap, facingIdentifier.id);
+	const isClassValid = attr.classTypes.indexOf(facingCard?.class) >= 0;
+
+	if (!isClassValid) return [emptyPassive, emptyPassive];
+
+	const payload = {
+		attack: attr.attack || 0,
+		defense: attr.defense || 0,
+		health: attr.health || 0,
+	};
+
+	return attr.isTargetEnemy ? [emptyPassive, payload] : [payload, emptyPassive];
+};

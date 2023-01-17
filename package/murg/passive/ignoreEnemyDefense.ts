09313f273546baedb5f9e205d629c6518a09ea8f
kayaba <fluctlight-kayaba@gmail.com>
2023-01-17 16:35:31 +0700

    correct ignoreEnemyDefense passive

--- package/murg/passive/ignoreEnemyDefense.ts
+++ package/murg/passive/ignoreEnemyDefense.ts
@@ -17,14 +17,14 @@ export const ignoreEnemyDefense: PassiveRunner = ({ duel, cardId }) => {
 	};
 
 	if (defense) {
-		enemyPassive.defense = Math.max(0, defense);
+		enemyPassive.defense = Math.min(0, -defense);
 	} else {
 		const state = getCardState(duel.stateMap, cardId);
 		const facingIdentifier = getFacingIdentifier(duel, state.owner, cardId);
 		const facingState = getCardState(duel.stateMap, facingIdentifier.id);
 		const facingDefense = facingState?.defense || 0;
 
-		enemyPassive.defense = Math.max(0, facingDefense);
+		enemyPassive.defense = Math.min(0, -facingDefense);
 	}
 
 	return [emptyPassive, enemyPassive];

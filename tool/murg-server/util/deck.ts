57de1b3d2073bad647035c9978b264e233b9a762
kayaba <fluctlight-kayaba@gmail.com>
2023-01-19 10:56:27 +0700

    filter skill-ready cards from server side (while create match)

--- tool/murg-server/util/deck.ts
+++ tool/murg-server/util/deck.ts
@@ -1,16 +1,18 @@
 import { CardMeta, CardType } from '@metacraft/murg-engine';
 
-export const generateRandomDeck = ({ map, entities }: CardMeta, size = 30) => {
-	const sources = [...entities];
+export const generateRandomDeck = (meta: CardMeta, size = 36) => {
+	const sources = pickSkillReadyCards(meta);
+	const maxSize = Math.floor(sources.length / 7);
+	const safeSize = Math.min(size, maxSize);
 	const results: string[] = [];
 	let count = 0;
 
-	while (count < size) {
+	while (count < safeSize) {
 		const randomIndex = Math.floor(Math.random() * sources.length);
 		const randomId = sources[randomIndex];
 		const sku = randomId.substring(0, 5);
 		const existedCard = results.find((id) => id.startsWith(sku));
-		const card = map[randomId];
+		const card = meta.map[randomId];
 
 		if (!existedCard && card.kind !== CardType.Troop) {
 			results.push(randomId);
@@ -21,4 +23,24 @@ export const generateRandomDeck = ({ map, entities }: CardMeta, size = 30) => {
 	}
 
 	return results;
+};
+
+const pickSkillReadyCards = ({ map, entities }: CardMeta) => {
+	const result = [];
+
+	for (let i = 0; i < entities.length; i += 1) {
+		const cardId = entities[i];
+		const card = map[cardId];
+		const isTroopCard = card.kind === CardType.Troop;
+		const isSkillValid =
+			card.skill.activation === undefined ||
+			card.skill.attribute ||
+			card.skill.passiveAttribute;
+
+		if (!isTroopCard && isSkillValid) {
+			result.push(cardId);
+		}
+	}
+
+	return result;
 };

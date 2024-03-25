c680e6c234594d3102c325ce6815cbe6a3f3bb8d
Namiscrea7or <pphnam21@clc.fitus.edu.vn>
2024-03-25 11:58:54 +0700

    Add bot for server

--- tool/murg-server/index.ts
+++ tool/murg-server/index.ts
@@ -2,7 +2,8 @@ import { nanoId } from '@metacraft/murg-engine';
 import express from 'express';
 import injectSocket from 'express-ws';
 import { sign, verify } from 'jsonwebtoken';
-
+import { selectBestMove } from './Bot/botBundle';
+import { DuelCommandBundle } from '@metacraft/murg-engine';
 import {
 	CommandPayload,
 	CommandResponse,
@@ -22,10 +23,15 @@ const socket = injectSocket(app);
 const duelClients: Record<string, Array<{ player: string; ws: unknown }>> = {};
 const jwtSecret = 'shh!!';
 
+
 app.ws('/', (ws) => {
 	ws.on('message', async (rawData) => {
 		try {
 			const data: CommandPayload = JSON.parse(rawData);
+			// const newData = data.payload as DuelCommandBundle
+			// newData.forEach(element => {
+			// 	console.log("Thiss something i know", element.commands)
+			// });
 			const { userId, duelId } = verify(data.jwt, jwtSecret) as JwtPayload;
 			const players = duelClients[duelId] || [];
 			const findPlayer = (i) => i.player === userId;
@@ -40,7 +46,7 @@ app.ws('/', (ws) => {
 						timestamp: new Date().getTime(),
 						payload,
 					};
-
+					//console.log("This is response@#@#$#@", response)
 					client.send(JSON.stringify(response));
 				});
 			};
@@ -53,10 +59,24 @@ app.ws('/', (ws) => {
 
 			if (data.command === DuelCommands.ConnectMatch) {
 				await onIncomingConnect(context, data.payload);
-			} else if (data.command === DuelCommands.SendBundle) {
+			} else if (data.command === DuelCommands.SendBundle) { // cần đổi chỗ này
 				await onIncomingBundle(context, data.payload);
+				console.log("Checking context and datapayload", context, "datapayload", data.payload)
 			} else if (data.command === DuelCommands.CardHover) {
 				await onInComingHover(context, data.payload);
+			}
+			// kiểm tra endturn.
+
+			if (data.payload[0].group === 'EndTurn' && data.payload[0].phaseOf === 'A') {
+				//console.log("It went Here!!!!!!!!!!!!!!!!!", data.payload[0].commands) // co vo day
+				const botContext: Context = { duelId, userId: 'B', command: DuelCommands.SendBundle, send }
+				//console.log("Maybe co chay vo day")// co chay vo day
+				let currentMoveBundle = selectBestMove(duelId, 1) // o day loi
+				//console.log("This is currentmove", currentMoveBundle) // undefine
+				currentMoveBundle.forEach(moves => {
+					console.log("Some move is here", botContext, "LALALAALAL", moves)
+					onIncomingBundle(context, moves)
+				});
 			}
 		} catch (e) {
 			await ws.send(JSON.stringify({ message: 'error' }));
